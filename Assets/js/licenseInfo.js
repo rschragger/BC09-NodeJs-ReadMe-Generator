0fc3d4bba2a35d4422f6043b2f6227e347859395
Reeve Schragger <80379457+rschragger@users.noreply.github.com>
2022-07-22 17:26:48 +1000

    Trying to branch on questions

---
+++ Assets/js/licenseInfo.js
@@ -0,0 +1,199 @@
+function licInf () {
+   return {
+   "Apache": [
+      {
+         "licOrg": [],
+         "licenseName": "Apache 2.0 License",
+         "licenseBadge": "[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)"
+      }
+   ],
+   "Boost": [
+      {
+         "licOrg": [],
+         "licenseName": "Boost Software License 1.0",
+         "licenseBadge": "[![License](https://img.shields.io/badge/License-Boost_1.0-lightblue.svg)](https://www.boost.org/LICENSE_1_0.txt)"
+      }
+   ],
+   "BSD": [
+      {
+         "licOrg": [],
+         "licenseName": "BSD 3-Clause License",
+         "licenseBadge": "[![License](https://img.shields.io/badge/License-BSD_3--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)"
+      },
+      {
+         "licOrg": [],
+         "licenseName": "BSD 2-Clause License",
+         "licenseBadge": "[![License](https://img.shields.io/badge/License-BSD_2--Clause-orange.svg)](https://opensource.org/licenses/BSD-2-Clause)"
+      }
+   ],
+   "Creative Commons": [
+      {
+         "licOrg": [],
+         "licenseName": "CC0",
+         "licenseBadge": "[![License: CC0-1.0](https://img.shields.io/badge/License-CC0_1.0-lightgrey.svg)](http://creativecommons.org/publicdomain/zero/1.0/)"
+      },
+      {
+         "licOrg": [],
+         "licenseName": "Attribution 4.0 International",
+         "licenseBadge": "[![License: CC BY 4.0](https://img.shields.io/badge/License-CC_BY_4.0-lightgrey.svg)](https://creativecommons.org/licenses/by/4.0/)"
+      },
+      {
+         "licOrg": [],
+         "licenseName": "Attribution-ShareAlike 4.0 International",
+         "licenseBadge": "[![License: CC BY-SA 4.0](https://img.shields.io/badge/License-CC_BY--SA_4.0-lightgrey.svg)](https://creativecommons.org/licenses/by-sa/4.0/)"
+      },
+      {
+         "licOrg": [],
+         "licenseName": "Attribution-NonCommercial 4.0 International",
+         "licenseBadge": "[![License: CC BY-NC 4.0](https://img.shields.io/badge/License-CC_BY--NC_4.0-lightgrey.svg)](https://creativecommons.org/licenses/by-nc/4.0/)"
+      },
+      {
+         "licOrg": [],
+         "licenseName": "Attribution-NoDerivates 4.0 International",
+         "licenseBadge": "[![License: CC BY-ND 4.0](https://img.shields.io/badge/License-CC_BY--ND_4.0-lightgrey.svg)](https://creativecommons.org/licenses/by-nd/4.0/)"
+      },
+      {
+         "licOrg": [],
+         "licenseName": "Attribution-NonCommmercial-ShareAlike 4.0 International",
+         "licenseBadge": "[![License: CC BY-NC-SA 4.0](https://img.shields.io/badge/License-CC_BY--NC--SA_4.0-lightgrey.svg)](https://creativecommons.org/licenses/by-nc-sa/4.0/)"
+      },
+      {
+         "licOrg": [],
+         "licenseName": "Attribution-NonCommercial-NoDerivatives 4.0 International",
+         "licenseBadge": "[![License: CC BY-NC-ND 4.0](https://img.shields.io/badge/License-CC_BY--NC--ND_4.0-lightgrey.svg)](https://creativecommons.org/licenses/by-nc-nd/4.0/)"
+      }
+   ],
+   "Eclipse": [
+      {
+         "licOrg": [],
+         "licenseName": "Eclipse Public License 1.0",
+         "licenseBadge": "[![License](https://img.shields.io/badge/License-EPL_1.0-red.svg)](https://opensource.org/licenses/EPL-1.0)"
+      }
+   ],
+   "GNU": [
+      {
+         "licOrg": [],
+         "licenseName": "GNU GPL v3",
+         "licenseBadge": "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)"
+      },
+      {
+         "licOrg": [],
+         "licenseName": "GNU GPL v2",
+         "licenseBadge": "[![License: GPL v2](https://img.shields.io/badge/License-GPL_v2-blue.svg)](https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html)"
+      },
+      {
+         "licOrg": [],
+         "licenseName": "GNU AGPL v3",
+         "licenseBadge": "[![License: AGPL v3](https://img.shields.io/badge/License-AGPL_v3-blue.svg)](https://www.gnu.org/licenses/agpl-3.0)"
+      },
+      {
+         "licOrg": [],
+         "licenseName": "GNU LGPL v3",
+         "licenseBadge": "[![License: LGPL v3](https://img.shields.io/badge/License-LGPL_v3-blue.svg)](https://www.gnu.org/licenses/lgpl-3.0)"
+      },
+      {
+         "licOrg": [],
+         "licenseName": "GNU FDL v1.3",
+         "licenseBadge": "[![License: FDL 1.3](https://img.shields.io/badge/License-FDL_v1.3-blue.svg)](https://www.gnu.org/licenses/fdl-1.3)"
+      }
+   ],
+   "The Organization for Ethical Source": [
+      {
+         "licOrg": [],
+         "licenseName": "The Hippocratic License 2.1",
+         "licenseBadge": " [![License: Hippocratic 2.1](https://img.shields.io/badge/License-Hippocratic_2.1-lightgrey.svg)](https://firstdonoharm.dev)"
+      },
+      {
+         "licOrg": [],
+         "licenseName": "The Hippocratic License 3.0",
+         "licenseBadge": " [![License: Hippocratic 3.0](https://img.shields.io/badge/License-Hippocratic_3.0-lightgrey.svg)](https://firstdonoharm.dev)"
+      }
+   ],
+   "IBM": [
+      {
+         "licOrg": [],
+         "licenseName": "IBM Public License Version 1.0",
+         "licenseBadge": "[![License: IPL 1.0](https://img.shields.io/badge/License-IPL_1.0-blue.svg)](https://opensource.org/licenses/IPL-1.0)"
+      }
+   ],
+   "ISC": [
+      {
+         "licOrg": [],
+         "licenseName": "ISC License (ISC)",
+         "licenseBadge": "[![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC)"
+      }
+   ],
+   "MIT": [
+      {
+         "licOrg": [],
+         "licenseName": "The MIT License",
+         "licenseBadge": "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)"
+      }
+   ],
+   "Mozilla": [
+      {
+         "licOrg": [],
+         "licenseName": "Mozilla Public License 2.0",
+         "licenseBadge": "[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)"
+      }
+   ],
+   "Open Data Commons": [
+      {
+         "licOrg": [],
+         "licenseName": "Attribution License (BY)",
+         "licenseBadge": "[![License: Open Data Commons Attribution](https://img.shields.io/badge/License-ODC_BY-brightgreen.svg)](https://opendatacommons.org/licenses/by/)"
+      },
+      {
+         "licOrg": [],
+         "licenseName": "Open Database License (ODbL)",
+         "licenseBadge": "[![License: ODbL](https://img.shields.io/badge/License-ODbL-brightgreen.svg)](https://opendatacommons.org/licenses/odbl/)"
+      },
+      {
+         "licOrg": [],
+         "licenseName": "Public Domain Dedication and License (PDDL)",
+         "licenseBadge": "[![License: ODbL](https://img.shields.io/badge/License-PDDL-brightgreen.svg)](https://opendatacommons.org/licenses/pddl/)"
+      }
+   ],
+   "Perl": [
+      {
+         "licOrg": [],
+         "licenseName": "The Perl License",
+         "licenseBadge": "[![License: Artistic-2.0](https://img.shields.io/badge/License-Perl-0298c3.svg)](https://opensource.org/licenses/Artistic-2.0)"
+      },
+      {
+         "licOrg": [],
+         "licenseName": "The Artistic License 2.0",
+         "licenseBadge": "[![License: Artistic-2.0](https://img.shields.io/badge/License-Artistic_2.0-0298c3.svg)](https://opensource.org/licenses/Artistic-2.0)"
+      }
+   ],
+   "SIL": [
+      {
+         "licOrg": [],
+         "licenseName": "SIL Open Font License 1.1",
+         "licenseBadge": "[![License: Open Font-1.1](https://img.shields.io/badge/License-OFL_1.1-lightgreen.svg)](https://opensource.org/licenses/OFL-1.1)"
+      }
+   ],
+   "Unlicense": [
+      {
+         "licOrg": [],
+         "licenseName": "The Unlicense",
+         "licenseBadge": "[![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)](http://unlicense.org/)"
+      }
+   ],
+   "WTFPL": [
+      {
+         "licOrg": [],
+         "licenseName": "The Do What the Fuck You Want to Public License",
+         "licenseBadge": "[![License: WTFPL](https://img.shields.io/badge/License-WTFPL-brightgreen.svg)](http://www.wtfpl.net/about/)"
+      }
+   ],
+   "Zlib": [
+      {
+         "licOrg": [],
+         "licenseName": "The zlib/libpng License",
+         "licenseBadge": "[![License: Zlib](https://img.shields.io/badge/License-Zlib-lightgrey.svg)](https://opensource.org/licenses/Zlib)"
+      }
+   ]
+}}
+
+module.exports = licInf()
--- index.js
+++ index.js
@@ -1,13 +1,89 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const licInfo = require('./assets/js/licenseInfo.js')
+
+
+
+//Variables
+var finalFileText = '';
+
+//Functions ---------
+function getLicenses(obj) {
+  let x = [];
+  for (let i = 0; i < obj.length; i++) {
+    x += licInfo['GNU'][i].licenseName
+  };
+  return x
+}
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+  // {
+  //   type: 'input',
+  //   name: 'fullName',
+  //   message: 'What is your full name? (Name Surname)',
+  // },
+  {
+    type: 'input',
+    name: 'gitHubName',
+    message: 'What is your GitHub ID?',
+  },
+  {
+    type: 'input',
+    name: 'projectName',
+    message: 'What is your Project Name?',
+  },
+  // {
+  //   type: 'checkbox',
+  //   message: 'What languages are being used in this project?',
+  //   name: 'languagesListed',
+  //   choices: ['HTML', 'CSS', 'JavaScript', 'MySQL', 'Other'],
+  // },
+  {
+    type: 'list',
+    message: 'What license will be used for this project?',
+    name: 'licenseOrg',
+    choices: Object.keys(licInfo),
+  },
+  //Use a when statement https://stackoverflow.com/questions/56412516/conditional-prompt-rendering-in-inquirer
+  {
+    type: 'list',
+    message: 'What license version would you prefer?',
+    name: 'license',
+    choices: Object.keys(getLicenses( licInfo[answers.licenseOrg])),
+    when: (answers) => {
+      let licOrgList = licInfo[answers.licenseOrg];
+      if (licOrgList.length > 0) {
+        return true
+      } else { return false };
+    }
+  },
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(data) {
+  const filename = `ReadMe_${data.projectName.toLowerCase().split(' ').join('')}.json`;
+
+  fs.writeFile(filename, JSON.stringify(data, null, '\t'), (err) =>
+    err ? console.log(err) : console.log('Success!')
+  );
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+  inquirer
+    .prompt(questions)
+    .then((data) => {
+
+      writeToFile(data);
+     
+    });
+}
 
 // Function call to initialize app
 init();
+
+
+
+
